c8d40bc60641f988380cb4ab4ab44e96c5953e58
WaifuXv <100534178+WaifuXv@users.noreply.github.com>
2022-06-24 22:09:23 -0500

    Add files via upload

---
+++ Ejercicio 2 OB/Ejercicio2.js
@@ -0,0 +1,21 @@
+//Ejercicio 2
+const nombre = "Juan Jose Garcia Solano"
+console.log(nombre);
+
+const años = 16
+console.log(años);
+
+const Eres_desarrollador = true
+console.log(Eres_desarrollador);
+
+const fecha_de_nacimiento = new Date(2005, 07, 27);
+console.log(fecha_de_nacimiento);
+
+// Este no es mi libro favorito
+const mi_libro_favorito = {
+    titulo: "The Pragmatic Programmer",
+    autor: "Andy Hunt y Dave Thomas",
+    fecha: 1999,
+    url: "https://www.amazon.com.mx/Pragmatic-Programmer-Journey-Mastery-Anniversary/dp/0135957052/ref=sr_1_1?adgrpid=129913118301&gclid=CjwKCAjwwdWVBhA4EiwAjcYJEP7p-Z18jazb0ANr2ml3CT_H691HPQZEQacPS7RH28UeSM0BPsSzShoCE3cQAvD_BwE&hvadid=580458777067&hvdev=c&hvlocphy=1031274&hvnetw=g&hvqmt=e&hvrand=1160526535845107747&hvtargid=kwd-302199567278&hydadcr=12466_13225846&keywords=the+pragmatic+programmer&qid=1656125024&s=books&sr=1-1"
+}
+console.log(mi_libro_favorito);
